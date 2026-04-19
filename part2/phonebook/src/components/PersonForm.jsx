a38a609d59c01acd0f245d5946f9f15bd305acdd
Oleh <oleh.obiukh@gmail.com>
2026-04-19 23:23:42 +0200

    part2: phonebook refactoring

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -8,24 +8,26 @@ const PersonForm = ({
   return (
     <form onSubmit={addPerson}>
       <table>
-        <tr>
-          <td>name:</td>
-          <td>
-            <input value={newName} onChange={handleNewName} />
-          </td>
-        </tr>
-        <tr>
-          <td>number:</td>
-          <td>
-            <input value={newNumber} onChange={handleNewNumber} />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button type="submit">add</button>
-          </td>
-          <td></td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>name:</td>
+            <td>
+              <input value={newName} onChange={handleNewName} />
+            </td>
+          </tr>
+          <tr>
+            <td>number:</td>
+            <td>
+              <input value={newNumber} onChange={handleNewNumber} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button type="submit">add</button>
+            </td>
+            <td></td>
+          </tr>
+        </tbody>
       </table>
     </form>
   );

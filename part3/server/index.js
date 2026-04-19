1439906f04d79b605c0e397f29036822c931c429
Oleh <oleh.obiukh@gmail.com>
2026-04-20 01:41:34 +0200

    3.3: /api/persons/:id

--- part3/server/index.js
+++ part3/server/index.js
@@ -37,6 +37,17 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.get("/api/info", (req, res) => {
   const time = new Date();
   const text = `

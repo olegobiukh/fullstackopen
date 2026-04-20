9590f4bb3331b4a467205b0a841fb39fc1fee143
Oleh <oleh.obiukh@gmail.com>
2026-04-20 19:30:00 +0200

    3.10: added dist folder

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -7,8 +7,8 @@ const app = express();
 
 
 app.use(cors());
-app.use(express.static('dist'));
 app.use(express.json());
+app.use(express.static('dist'));
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : '';

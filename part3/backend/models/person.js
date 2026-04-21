6cfdddb3040bcf421c901de2eec6a9adfb618b6e
Oleh <oleh.obiukh@gmail.com>
2026-04-21 02:20:20 +0200

    3.20*: custom number validation

--- part3/backend/models/person.js
+++ part3/backend/models/person.js
@@ -20,8 +20,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 6,
-    required: true,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{6,8}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
   },
 });
 

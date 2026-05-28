7325c0fd8678c4f39b4f39987d869b8ee07416ae
Oleh Obiukh <oleg.obiukh@gmail.com>
2026-05-28 23:41:47 +0200

    Update the Note Model

--- part4/bloglist-backend/models/blog.js
+++ part4/bloglist-backend/models/blog.js
@@ -3,17 +3,21 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   author: String,
   url: {
     type: String,
-    required: true
+    required: true,
   },
   likes: {
     type: Number,
-    default: 0
-  }
+    default: 0,
+  },
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User" 
+  },
 });
 
 blogSchema.set("toJSON", {

6c22dbe2150531a236b03856668026eb8a184d4a
Oleh Obiukh <oleg.obiukh@gmail.com>
2026-05-28 23:53:15 +0200

    4c: Password validation

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -4,6 +4,7 @@ const logger = require("./utils/logger");
 const cors = require("cors");
 const { MONGODB_URI, PORT } = require("./utils/config");
 const blogRouter = require("./controllers/blogs");
+const usersRouter = require('./controllers/users')
 const middleware = require("./utils/middleware");
 
 const app = express();
@@ -17,11 +18,12 @@ mongoose
     logger.error("error connecting to MongoDB:", error.message);
   });
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(middleware.requestLogger);
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ part4/bloglist-backend/controllers/users.js
@@ -0,0 +1,37 @@
+const User = require("../models/user");
+const usersRouter = require("express").Router();
+const bcrypt = require("bcrypt");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  if (!password || password.length < 3) {
+    return response
+      .status(400)
+      .json({ error: "password must be at least 3 characters long" });
+  }
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+usersRouter.delete("/:id", async (request, response) => {
+  await User.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+module.exports = usersRouter;
